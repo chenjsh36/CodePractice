4a81ca91d7bb757db1b51c515f90c876b7663fd0
chenjsh36 <chenjsh36@outlook.com>
2017-08-10 19:03:09 +0800

    update

---
+++ test/test_example.js
@@ -0,0 +1,10 @@
+var assert = require('assert')
+var example = require('../src/example.js')
+
+describe('Add func test', function() {
+    describe('func add()', function() {
+        it('should be 2 when 1 add 1', function() {
+            assert.equal(example.add(1, 1), 2)
+        })
+    })
+})
